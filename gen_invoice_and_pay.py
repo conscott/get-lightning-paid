acc415b1e2109b4f517bf0b0dfbb2bb97b5a195f
Conor Scott <conor.r.scott.88@gmail.com>
2018-11-20 23:10:36 +0400

    Add comments to test script

--- gen_invoice_and_pay.py
+++ gen_invoice_and_pay.py
@@ -8,21 +8,20 @@
 
 import requests
 
+# Point to running flask app
 host = 'http://localhost:5000'
 
+# POST to make an invoice
 data = {'msatoshi': 12000, 'description': 'test invoice'}
-
 r = requests.post(host + '/api/generate_invoice', json=data)
-
 assert r.status_code == 200
-
 invoice_data = r.json()
-
 print("Generated invoice:\n\n%s\n" % invoice_data['bolt11'])
+
+# Now have to externally pay the invoice while endpoint waits
+# for payment confirmation
 print("Waiting for payment....")
-
 r = requests.get(host + "/api/wait_invoice/%s" % invoice_data['label'])
-
 assert r.status_code == 200
 
 print("Paid!")
