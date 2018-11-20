50fd47e8749d424065dd663de3e1fd495a2cda17
Conor Scott <conor.r.scott.88@gmail.com>
2018-11-20 23:10:23 +0400

    Add CORS and test endpoints

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from lightning import LightningRpc
 import os
 import random
 
 # Setup flask app object
 app = Flask(__name__)
+CORS(app)
 
 
 def default_configdir():
@@ -45,7 +47,7 @@ def make_invoice():
     assert 'msatoshi' in data
     amount = data['msatoshi']
     description = data.get('description', '')
-    expiry = data.get('expiry', 3600)
+    expiry = data.get('expiry', 600)
     label = label_generator()
     invoice = ld.invoice(amount, label, description, expiry)
     # Add label so you can check status through API
@@ -89,6 +91,35 @@ def list_invoices():
 @app.route('/api/getinfo')
 def getinfo():
     return jsonify(ld.getinfo())
+
+#
+# Test API
+#
+#
+
+
+# Get a fake bolt invoice
+@app.route('/test/generate_invoice', methods=['POST'])
+def test_invoice():
+    data = request.get_json()
+    assert 'msatoshi' in data
+    data = {
+        "payment_hash": "fake_hash",
+        "expires_at": 234132412,
+        "bolt11": "asdlfjaljfdlsajfdlsajdflsajdflksajdflasjdflksajdflksajfdlksajasdflkjasdfasdfasdfasdfasdfasdfsadfasdfasdfsadfsadfsadfsadfsadfasdfasdfsadfsadfsadfsadfafffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+        "label": "fake_label"
+    }
+    return jsonify(data)
+
+
+# Check on fake invoice
+#
+# endpoint will just waits 5 seconds and return
+@app.route("/test/check_invoice/<label>", methods=['GET'])
+def check_fake_invoice(label):
+    import time
+    time.sleep(5)
+    return jsonify({'status': 'paid'})
 
 
 if __name__ == "__main__":
